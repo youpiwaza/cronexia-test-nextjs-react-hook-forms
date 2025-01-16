e4b590e5f23b18fe69f4a3f34310c2b1d4f46353
youpiwaza <masamune.code@gmail.com>
2025-01-16 12:33:31 +0100

    📌ADD: NestJs & React hook forms

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
 # cronexia-test-nextjs-react-hook-forms
+
+
+yep
+
+[doc](https://nextjs.org/docs/app/getting-started/installation)
+
+```bash
+npx create-next-app@latest project
+cd project/
+bun dev
+```
+
+[doc](https://react-hook-form.com/get-started)
+
+```bash
+bun add react-hook-form
+```
+
+HS / The export useForm was not found in module
+
+lié a [typescript ?](https://react-hook-form.com/ts) d'après so
+
+https://stackoverflow.com/questions/72144671/how-to-resolve-the-error-module-react-hook-form-has-no-exported-member-use
+
+Besoin de 
+
+```ts
+'use client';
+```
+
+en début de fichier
+
+https://github.com/react-hook-form/react-hook-form/issues/11284#issuecomment-1850995149
+https://react.dev/reference/rsc/use-client
---
+++ project/src/app/page.tsx
@@ -0,0 +1,138 @@
+'use client';
+
+import Image from "next/image";
+import styles from "./page.module.css";
+
+// https://react-hook-form.com/ts
+import React from "react"
+import { useForm, Resolver } from "react-hook-form"
+
+type FormValues = {
+  firstName: string
+  lastName: string
+}
+
+const resolver: Resolver<FormValues> = async (values) => {
+  return {
+    values: values.firstName ? values : {},
+    errors: !values.firstName
+      ? {
+          firstName: {
+            type: "required",
+            message: "This is required.",
+          },
+        }
+      : {},
+  }
+}
+
+export default function Home() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormValues>({ resolver })
+  const onSubmit = handleSubmit((data) => console.log(data))
+
+  return (
+    <div className={styles.page}>
+      <main className={styles.main}>
+        <Image
+          className={styles.logo}
+          src="/next.svg"
+          alt="Next.js logo"
+          width={180}
+          height={38}
+          priority
+        />
+        <ol>
+          <li>
+            TEST <a href="https://react-hook-form.com/">REACT HOOK FORM</a> <code>src/app/page.tsx</code>.
+          </li>
+          <li>Save and see your changes instantly.</li>
+        </ol>
+
+        <div>
+          <form onSubmit={onSubmit}>
+            <input {...register("firstName")} placeholder="Bill" />
+            {errors?.firstName && <p>{errors.firstName.message}</p>}
+
+            <input {...register("lastName")} placeholder="Luo" />
+
+            <input type="submit" />
+          </form>
+        </div>
+
+        <div className={styles.ctas}>
+          <a
+            className={styles.primary}
+            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Image
+              className={styles.logo}
+              src="/vercel.svg"
+              alt="Vercel logomark"
+              width={20}
+              height={20}
+            />
+            Deploy now
+          </a>
+          <a
+            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+            className={styles.secondary}
+          >
+            Read our docs
+          </a>
+        </div>
+      </main>
+      <footer className={styles.footer}>
+        <a
+          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="/file.svg"
+            alt="File icon"
+            width={16}
+            height={16}
+          />
+          Learn
+        </a>
+        <a
+          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="/window.svg"
+            alt="Window icon"
+            width={16}
+            height={16}
+          />
+          Examples
+        </a>
+        <a
+          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="/globe.svg"
+            alt="Globe icon"
+            width={16}
+            height={16}
+          />
+          Go to nextjs.org →
+        </a>
+      </footer>
+    </div>
+  );
+}
